67e1527e95044e20fa1e21fad1d619ed48264a14
ESAClub <45108933+ESAClub@users.noreply.github.com>
2018-11-17 09:13:23 -0500

    Update and rename temperature-2018-11-09-16-37-04.py to sensehat_temperature_example.py

--- sensehat_temperature_example.py
+++ sensehat_temperature_example.py
@@ -1,5 +1,4 @@
-# This file has been written to your home directory for convenience. It is
-# saved as "/home/pi/temperature-2018-11-09-16-37-04.py"
+# SenseHat temperature and matrix example
 
 from sense_emu import SenseHat
 
